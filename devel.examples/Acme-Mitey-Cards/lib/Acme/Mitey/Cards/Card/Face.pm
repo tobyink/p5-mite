c63458d3242cf42c57671d5fa6bf221da31914a7
Toby Inkster <mail@tobyinkster.co.uk>
2022-07-03 00:41:40 +0100

    bump version; changelog

--- devel.examples/Acme-Mitey-Cards/lib/Acme/Mitey/Cards/Card/Face.pm
+++ devel.examples/Acme-Mitey-Cards/lib/Acme/Mitey/Cards/Card/Face.pm
@@ -1,6 +1,6 @@
 package Acme::Mitey::Cards::Card::Face;
 
-our $VERSION   = '0.005';
+our $VERSION   = '0.006';
 our $AUTHORITY = 'cpan:TOBYINK';
 
 use Acme::Mitey::Cards::Mite;
--- devel.examples/Acme-Mitey-Cards/lib/Acme/Mitey/Cards/Suit.pm
+++ devel.examples/Acme-Mitey-Cards/lib/Acme/Mitey/Cards/Suit.pm
@@ -1,6 +1,6 @@
 package Acme::Mitey::Cards::Suit;
 
-our $VERSION   = '0.005';
+our $VERSION   = '0.006';
 our $AUTHORITY = 'cpan:TOBYINK';
 
 use Acme::Mitey::Cards::Mite;
