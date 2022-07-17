3f1c8631e79266b85753bd4591852980ffab206c
Toby Inkster <mail@tobyinkster.co.uk>
2022-07-17 10:52:42 +0100

    Rebuild with recent Mite

--- devel.examples/Acme-Mitey-Cards/lib/Acme/Mitey/Cards/Set.pm.mite.pm
+++ devel.examples/Acme-Mitey-Cards/lib/Acme/Mitey/Cards/Set.pm.mite.pm
@@ -6,7 +6,7 @@
 
     our $USES_MITE    = "Mite::Class";
     our $MITE_SHIM    = "Acme::Mitey::Cards::Mite";
-    our $MITE_VERSION = "0.006012";
+    our $MITE_VERSION = "0.007002";
 
     BEGIN {
         *bare  = \&Acme::Mitey::Cards::Mite::bare;
