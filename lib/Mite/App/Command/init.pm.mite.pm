7c6d7a62ee11f1eb1a81e684bebac7ade8350763
Toby Inkster <mail@tobyinkster.co.uk>
2022-08-03 15:16:47 +0100

    bump version; changelog

--- lib/Mite/App/Command/init.pm.mite.pm
+++ lib/Mite/App/Command/init.pm.mite.pm
@@ -6,7 +6,7 @@
 
     our $USES_MITE    = "Mite::Class";
     our $MITE_SHIM    = "Mite::Shim";
-    our $MITE_VERSION = "0.008000";
+    our $MITE_VERSION = "0.008001";
 
     BEGIN {
         require Scalar::Util;
