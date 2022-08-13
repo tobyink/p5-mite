810532481bec900e9df3a9a2f137939c6ab960ca
Toby Inkster <mail@tobyinkster.co.uk>
2022-08-13 09:23:21 +0100

    Already disabled these warnings in a wider scope

--- lib/Mite/Package.pm
+++ lib/Mite/Package.pm
@@ -225,7 +225,6 @@ sub _compile_meta_method {
 # Gather metadata for constructor and destructor
 sub __META__ {
     no strict 'refs';
-    no warnings 'once';
     my $class      = shift; $class = ref($class) || $class;
     my $linear_isa = mro::get_linear_isa( $class );
     return {
